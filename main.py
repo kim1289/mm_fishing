ce5f5ac9e08589cfc4601af24b71f0f2897f3f79
kim1289 <rlawngus129@naver.com>
2025-06-15 11:08:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
                 print('쓰레기가 낚였습니다\n재시작 합니다\n')
                 continue
 
-            sleep(4)
+            sleep(5.7)
 
             res = hook.hooking_fish()
             if res:
